df751bf4398f84a267e119d28150bfebc8df1f94
ahyoung <jr3537@naver.com>
2025-07-08 15:15:24 +0900

    Feat: LectureMember Entity 구현
    Resolves: #3

---
+++ LectureFlow/src/main/java/github/nbcamp/lectureflow/global/entity/LectureMember.java
@@ -0,0 +1,31 @@
+package github.nbcamp.lectureflow.global.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LectureMember extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //수강신청 내역 고유 ID
+    private Long id;
+
+    //학생(member)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    //강의 (lecture)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id", nullable = false)
+    private Lecture lecture;
+
+    // 재수강 여부
+    @Column(nullable=false)
+    private boolean isRetake=false;
+}
