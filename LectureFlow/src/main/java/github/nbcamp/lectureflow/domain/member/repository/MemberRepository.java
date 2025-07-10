cc5f5e7143d4b856bc37a3cb293f4c3603eaf6da
hoojun99@naver.com <hoojun99@naver.com>
2025-07-10 20:03:22 +0900

    Feat: 기본 MemberRepository 구현(extends JpaRepository)

--- LectureFlow/src/main/java/github/nbcamp/lectureflow/domain/member/repository/MemberRepository.java
+++ LectureFlow/src/main/java/github/nbcamp/lectureflow/domain/member/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package github.nbcamp.lectureflow.domain.member.repository;
 
-public class MemberRepository {
+import github.nbcamp.lectureflow.global.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
