52158ca26068622dd7d3b679493853a65212cc0e
Ddiy0ng <bueong0627@gmail.com>
2025-07-10 09:50:40 +0900

    Feat: 엑셀파일 DB에 업로드 구현
    Resolves: #11

---
+++ LectureFlow/src/main/java/github/nbcamp/lectureflow/domain/lecture/dto/LectureRequestDto.java
@@ -0,0 +1,30 @@
+package github.nbcamp.lectureflow.domain.lecture.dto;
+
+import github.nbcamp.lectureflow.global.Response.LectureResponse;
+import github.nbcamp.lectureflow.global.entity.Lecture;
+import github.nbcamp.lectureflow.global.enums.Day;
+import github.nbcamp.lectureflow.global.enums.Department;
+import github.nbcamp.lectureflow.global.enums.MajorOrGeneral;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalTime;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class LectureRequestDto {
+    private MajorOrGeneral majorOrGeneral;
+    private Department department;
+    private int gradeLevel;
+    private boolean isForeignLanguage;
+    private String lectureName;
+    private int grade;
+    private String professor;
+    private Day day;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int classroom;
+    private int maxStudent;
+
+}
