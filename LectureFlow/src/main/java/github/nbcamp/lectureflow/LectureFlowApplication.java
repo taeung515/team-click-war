540c75e6bc5f648ed4dfc865e7d8f6e277d4e456
Gyum1 <xoruadl123@gmail.com>
2025-07-14 15:30:22 +0900

    feat: CD 테스트용

--- LectureFlow/src/main/java/github/nbcamp/lectureflow/LectureFlowApplication.java
+++ LectureFlow/src/main/java/github/nbcamp/lectureflow/LectureFlowApplication.java
@@ -2,10 +2,12 @@ package github.nbcamp.lectureflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+@EnableCaching
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class LectureFlowApplication {
