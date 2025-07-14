1eb93c86c0c66ad4712781501f4355d632fa0ad1
taeung515 <xodndd515@gmail.com>
2025-07-14 23:31:51 +0900

    feat: 캐시 동기화 문제 해결 - TTL 30분 설정 - @CacheEvict, @Scheduled 주기적으로 캐시 무효화 로직 구현 - 학습 차원에서 둘다 적용 Resolve: #36

--- LectureFlow/src/main/java/github/nbcamp/lectureflow/global/config/RedisConfig.java
+++ LectureFlow/src/main/java/github/nbcamp/lectureflow/global/config/RedisConfig.java
@@ -3,22 +3,36 @@ package github.nbcamp.lectureflow.global.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
+    private static final int CACHE_TTL = 60 * 30; // 30 분
+
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                        new GenericJackson2JsonRedisSerializer()
-                ));
+                .disableCachingNullValues() // null 캐싱 X
+                .entryTtl(Duration.ofSeconds(CACHE_TTL))
+                .computePrefixWith(CacheKeyPrefix.simple())
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new StringRedisSerializer()))
+
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
